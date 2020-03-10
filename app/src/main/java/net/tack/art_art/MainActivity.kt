ce431c429953d7fa354c1c7993e9a21642c53c14
tack-ayumu <gaze696@gmail.com>
2020-03-11 00:19:36 +0900

    first

---
+++ app/src/main/java/net/tack/art_art/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.tack.art_art
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
