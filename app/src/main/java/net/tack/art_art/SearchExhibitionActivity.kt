533d160df3ce346728b87688cdf7be171817aa3b
tack-ayumu <gaze696@gmail.com>
2020-04-05 20:33:45 +0900

    (実験中）地域選択した際のカッコを外す

--- app/src/main/java/net/tack/art_art/SearchExhibitionActivity.kt
+++ app/src/main/java/net/tack/art_art/SearchExhibitionActivity.kt
@@ -21,11 +21,26 @@ class SearchExhibitionActivity : AppCompatActivity() {
         )
         spinner_area.adapter = arrayAdapter
 
+
        //会期検索（日付検索）date-picker
         val calender = java.util.Calendar.getInstance()
         val year = calender.get(java.util.Calendar.YEAR)
         val month = calender.get(java.util.Calendar.MONTH)
+        val month_plusone = month+1
         val day = calender.get(java.util.Calendar.DAY_OF_MONTH)
+
+        //日付設定を「本日」でデフォルト表示（月と日は一桁の場合、頭に「０」をつける設定）
+        textView_year.text = year.toString()
+        textView_month.text = month_plusone.toString()
+        when(month_plusone){
+            in 1..8 -> textView_month.text = "0"+ month_plusone.toString()
+        }
+        textView_day.text = day.toString()
+        when(day){
+            in 1..9 -> textView_day.text = "0"+ day.toString()
+        }
+
+
         val dtp = DatePickerDialog(this,DatePickerDialog.OnDateSetListener{ view, y, m, d ->
             textView_year.text = "$y"
             textView_month.text = "${m+1}"
@@ -40,11 +55,32 @@ class SearchExhibitionActivity : AppCompatActivity() {
             dtp.show()
         }
 
+        fun selected_year(){
 
-        go_search_exhibition.setOnClickListener {
-            textView2.text = " https://artscape.jp/exhibition/schedule/exhi_schedule_result.php=?" + spinner_area.selectedItem + "&Year="+
-                    textView_year.text.subSequence(0, 4)+"&Month="+textView_month.text.subSequence(0,2)+"&Day="+
-                    textView_day.text.subSequence(0,2)+"&period=2&selorder=1&search=&btn_submit=&f_submit=on"
+            val selected_year = textView_year.text.toString().substring(0,4)
+
+        }
+
+
+
+        go_search_exhibition.setOnClickListener() {
+            var selected_area = spinner_area.selectedItem.toString()
+            val idx = spinner_area.selectedItemPosition
+            when(idx){
+              3 -> selected_area = spinner_area.selectedItem.toString().substring(1,3)
+            }
+
+
+            val selected_year = textView_year.text.toString().substring(0,4)
+            val selected_month = textView_month.text.toString().substring(0,2)
+            val selected_day = textView_day.text.toString().substring(0,2)
+            val urlArtscape1 = "https://artscape.jp/exhibition/schedule/exhi_schedule_result.php?pref="
+            val urlArtscape2 = "&Year="
+            val urlArtscape3 = "&Month="
+            val urlArtscape4 = "&Day="
+            val urlArtscape5 = "&period=2&selorder=1&search=&btn_submit=&f_submit=on"
+
+            textView2.text = urlArtscape1 + selected_area + urlArtscape2 + selected_year + urlArtscape3 + selected_month+urlArtscape4 + selected_day + urlArtscape5
         }
 
 
